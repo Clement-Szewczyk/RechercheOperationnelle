0d26a21a7e4b21b3a0b70a5517193ad57853ca0f
Clement-Szewczyk <93139573+Clement-Szewczyk@users.noreply.github.com>
2024-12-26 11:55:00 +0100

    graphe colorié et évaluation

--- welsh_Powell.py
+++ welsh_Powell.py
@@ -7,46 +7,83 @@ def welsh_Powell(graphe):
     Algorithme de coloration de graphe de Welsh-Powell.
 
     :param graphe: Dictionnaire représentant le graphe.
-    :return: Dictionnaire d'un graphe coloré.
+    :return: Dictionnaire d'une coloration {sommet: couleur}.
     """
     # tri décroissant des sommets par degrés
-
     Sommet_trié = sorted(graphe, key=lambda sommet: len(graphe[sommet]), reverse=True)
-    print(Sommet_trié)
-
-    graphes_colores = {}
+    # Dictionaire de coloration
+    coloration = {}
     couleur = 0
 
+    # Parcours des sommets triés
     for sommet in Sommet_trié:
+
         couleur_disponible = True
+
+        # Parcours des voisins du sommet
         for voisin in graphe[sommet]:
-            if voisin in graphes_colores and graphes_colores[voisin] == couleur:
+            # Si le voisin a la même couleur que le sommet
+            if voisin in coloration and coloration[voisin] == couleur:
                 couleur_disponible = False
                 break
 
         if couleur_disponible:
-            graphes_colores[sommet] = couleur
+            coloration[sommet] = couleur
         else:
             couleur += 1
-            graphes_colores[sommet] = couleur
+            coloration[sommet] = couleur
     
-    return graphes_colores
+    return coloration
 
 
-def verifier_Coloration(graphe):
-    pass 
 
+def graphe_colorie(graphe, coloration):
+    """
+    Colorie un graphe selon une coloration donnée.
+
+    :param graphe: Dictionnaire représentant le graphe.
+    :param coloration: Dictionnaire {sommet: couleur}.
+    :return: Dictionnaire d'un graphe coloré avec la strucutre {Sommet: (couleur, [voisins])}.
+    """
+    graphe_colore = {}
+
+    for sommet, couleur in coloration.items():
+        graphe_colore[sommet] = (couleur, graphe[sommet])
+
+    return graphe_colore
+
+
+def evaluer_coloration(graphe_colore):
+    """
+    Évalue la qualité d'une coloration.
+
+    :param graphe_colore: Dictionnaire d'un graphe coloré avec la strucutre {Sommet: (couleur, [voisins])}.
+    :return: Nombre de conflits dans la coloration.
+    """
+    conflits = 0
+
+    for sommet, (couleur, voisins) in graphe_colore.items():
+        for voisin in voisins:
+            if couleur == graphe_colore[voisin][0]:
+                conflits += 1
+
+    return conflits // 2  # Chaque conflit est compté deux fois
+    
 
 graphe = {}
 fichier = "graphe/test1.col"
 
-
 graphe  = ls.lire_graphe_col(fichier)
+print("Graphe : ", graphe)
 
-ls.afficher_graphe(graphe)
+coloration = welsh_Powell(graphe)
 
-print(welsh_Powell(graphe))
+print("Coloration : ", coloration)
 
+graphe_colore = graphe_colorie(graphe, coloration)
+print("Graphe coloré : ", graphe_colore)
 
+score = evaluer_coloration(graphe_colore)
+print("Score : ", score)
 
 
