bddae99c0304dd4c1ab185c917306d4ee3359731
Clement-Szewczyk <93139573+Clement-Szewczyk@users.noreply.github.com>
2025-01-06 20:18:16 +0100

    update main.py
    Ajout modification d'un graphe

--- main.py
+++ main.py
@@ -9,26 +9,56 @@ def main():
     print("Projet Coloration de Graphe")
     print("Auteur : Jade Delebecque et Clément Szewczyk")
 
-    print("\n")
+    # Lecture d'un fichier de graphe modification
+
     fichier = "graphe/test1.col"
     print("Lecture du fichier : ", fichier)
-
+    
     graphe = ls.lire_graphe_col(fichier)
     print("Nombre de sommets :", len(graphe))
     print("Nombre d'arêtes :", sum(len(voisins) for voisins in graphe.values()) // 2)
+
+    # Affichage du graphe
+    print(graphe)
+    
+    # Ajout d'un sommet
+    print("Ajout d'un sommet")
+    ls.ajouter_Sommet(graphe, 6)
+    print(graphe)
+
+    # Ajout d'une arête
+    print("Ajout d'une arête")
+    ls.ajouter_Arete(graphe, 1, 6)
+    print(graphe)
+
+    # Suppression d'un Sommet
+    print("Suppression d'un sommet")
+    ls.supprimer_Sommet(graphe, 6)
+
+    print(graphe)
+
+
+
+    print("\n")
+    fichier = "graphe/anna.col"
+    print("Lecture du fichier : ", fichier)
+
+    graphe_Welsh_Powell = ls.lire_graphe_col(fichier)
+    print("Nombre de sommets :", len(graphe_Welsh_Powell))
+    print("Nombre d'arêtes :", sum(len(voisins) for voisins in graphe_Welsh_Powell.values()) // 2)
     print("\n")
 
     # Welsh-Powell
 
     print("Coloration de graphe avec Welsh-Powell")
-    coloration_Welsh_Powell, temps = wp.welsh_Powell(graphe)
+    coloration_Welsh_Powell, temps = wp.welsh_Powell(graphe_Welsh_Powell)
 
     print("Temps d'exécution : ", temps, " secondes")
     nb_couleur = wp.nb_couleur(coloration_Welsh_Powell)
     print("Nombre de couleurs : ", nb_couleur)
-    conflit_Welsh_Powell = ls.evaluer_coloration(ls.graphe_colorie(graphe, coloration_Welsh_Powell))
+    conflit_Welsh_Powell = ls.evaluer_coloration(ls.graphe_colorie(graphe_Welsh_Powell, coloration_Welsh_Powell))
     print("Conflit du Welsh Powell: ", conflit_Welsh_Powell)
-    graphe_colore_Welsh_Powell = ls.graphe_colorie(graphe, coloration_Welsh_Powell)
+    graphe_colore_Welsh_Powell = ls.graphe_colorie(graphe_Welsh_Powell, coloration_Welsh_Powell)
     print("\n")
     
     # Exportation d'un graphe coloré
